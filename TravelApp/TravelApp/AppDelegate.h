026a16579a8030338973d20a4eacb60546faed97
Tristan <kazuya-pp@hotmail.com>
2013-08-21 16:06:13 +1000

    Initial commit

---
+++ TravelApp/TravelApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TravelApp
+//
+//  Created by Tristan on 21/08/13.
+//  Copyright (c) 2013 Tristan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TravelApp/TravelApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TravelApp
+//
+//  Created by Tristan on 21/08/13.
+//  Copyright (c) 2013 Tristan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
